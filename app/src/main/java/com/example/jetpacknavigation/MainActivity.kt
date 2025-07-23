ce398c2eb061c7f8d9e8c909657dff57f62319b4
ganesh <ganeshaguru07@gmail.com>
2025-07-23 22:22:58 +0530

    first commit

---
+++ app/src/main/java/com/example/jetpacknavigation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jetpacknavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAppNavigation()
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DefaultPreview(){
+
+}
+
---
+++ app/src/main/java/com/example/jetpacknavigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.jetpacknavigation
+
+object Routes {
+    var screenA = "screen_A"
+    var screenB = "screen_B"
+}
